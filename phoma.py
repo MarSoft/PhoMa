6f23f8b73f92d64a05d55c5f205a08a886db1ce8
Semyon Maryasin <simeon@maryasin.name>
2016-10-18 04:07:22 +0300

    WIP port choosing

--- phoma.py
+++ phoma.py
@@ -1,3 +1,38 @@
+#!/usr/bin/env python
+from argparse import ArgumentParser
+from random import randrange
+import socket
+
 import muffin
 
 app = muffin.Application(__name__)
+
+@app.register('/')
+async def index(req):
+    pass
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-H', '--host', default='127.0.0.1',
+                        help='Host to bind to (defaults to localhost)')
+    parser.add_argument('-p', '--port', type=int,
+                        help='Port to bind to (defaults to random)')
+    args = parser.parse_args()
+
+    while True:
+        port = args.port or randrange(1000, 99999)
+        # check if port is available
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sock.bind((args.host, port))
+        except OSError:
+            # address is already in use?
+            if args.port:
+                parser.error('Port is busy')
+        else:
+            sock.close()
+            break
+
+    app.manage.handlers['run'](
+        bind='{}:{}'.format(args.host, port),
+    )
