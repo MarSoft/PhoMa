8976fa99e47ce8f74a505113a4ebfaeb7fef57ee
Semyon Maryasin <simeon@maryasin.name>
2016-10-21 19:44:24 +0300

    added list_directory_page method to protocol

--- protocol.py
+++ protocol.py
@@ -1,6 +1,15 @@
+from itertools import islice
+
 class Protocol:
     def list_directory(self):
         raise NotImplementedError
+
+    def list_directory_page(self, page, pagesize):
+        start = page * pagesize
+        return islice(
+            self.list_directory(),
+            start, start + pagesize,
+        )
 
     def get_files(self, names):
         # default implementation
