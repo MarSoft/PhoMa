f6abfd4edf99840dbfe3d15ee1923a5971c8cee1
Semyon Maryasin <simeon@maryasin.name>
2016-10-21 19:33:00 +0300

    Implemented browser starting

--- phoma.py
+++ phoma.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 from random import randrange
 import socket
 import logging
+import subprocess as sp
 
 import muffin
 from aiohttp import web
@@ -46,6 +47,8 @@ if __name__ == '__main__':
                         help='Host to bind to (defaults to localhost)')
     parser.add_argument('-p', '--port', type=int,
                         help='Port to bind to (defaults to random)')
+    parser.add_argument('-s', '--service', action='store_true',
+                        help='Don\'t start browser')
     args = parser.parse_args()
 
     while True:
@@ -64,6 +67,18 @@ if __name__ == '__main__':
             sock.close()
             print('Will listen on port %d' % port)
             break
+
+    if not args.service:
+        @app.register_on_start
+        def run_browser(app):
+            url = 'http://{}:{}/'.format(args.host, port)
+            try:
+                sp.check_call(['xdg-open', url])
+            except sp.CalledProcessError:
+                try:
+                    sp.check_call(['open', url])
+                except sp.CalledProcessError:
+                    print('Failed to open url')
 
     app.loop.run_until_complete(app.start())
     web.run_app(app, host=args.host, port=port)
